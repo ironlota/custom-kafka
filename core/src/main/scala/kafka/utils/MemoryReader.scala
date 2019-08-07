7316d129440a1ba175a00ef7a3c57b1826ce6dd8
Ray Andrew <raydreww@gmail.com>
2019-08-07 15:52:12 +0700

    continous memory reader

--- core/src/main/scala/kafka/utils/MemoryReader.scala
+++ core/src/main/scala/kafka/utils/MemoryReader.scala
@@ -8,34 +8,34 @@ class MemoryReader extends Runnable with Logging {
   private val MegaBytes = 1024 * 1024
 
   override def run(): Unit = {
-    while (!Thread.currentThread().isInterrupted()) {
-      try {
-          System.gc()
+    try {
+      while (!Thread.currentThread().isInterrupted()) {
+        System.gc()
 
-          // sleep for 5 seconds
-          Thread.sleep(5000)
+        // sleep for 5 seconds
+        Thread.sleep(5000)
 
-          if (Files.exists(Paths.get("/mnt/extra/holder.txt"))) {
+        if (Files.exists(Paths.get("/mnt/extra/holder.txt"))) {
 
-            val runtime = Runtime.getRuntime
+          val runtime = Runtime.getRuntime
 
-            val freeMemory = runtime.freeMemory / MegaBytes
-            val totalMemory = runtime.totalMemory / MegaBytes
-            val maxMemory = runtime.maxMemory / MegaBytes
+          val freeMemory = runtime.freeMemory / MegaBytes
+          val totalMemory = runtime.totalMemory / MegaBytes
+          val maxMemory = runtime.maxMemory / MegaBytes
 
 
-            info("Used Memory  : %s MB".format(totalMemory - freeMemory))
-            info("Free Memory  : %s MB".format(freeMemory))
-            info("Total Memory : %s MB".format(totalMemory))
-            info("Max Memory   : %s MB".format(maxMemory))
-          } else {
-            warn("Waiting for file exists")
-          }
-        } 
-      catch  {
-        case interrupt: InterruptedException => error("Error while sleeping")
-        case unknown: Throwable => error("Got this unknown exception: " + unknown)
-      }
+          info("Used Memory  : %s MB".format(totalMemory - freeMemory))
+          info("Free Memory  : %s MB".format(freeMemory))
+          info("Total Memory : %s MB".format(totalMemory))
+          info("Max Memory   : %s MB".format(maxMemory))
+        } else {
+          warn("Waiting for file exists")
+        }
+      } 
+    }
+    catch  {
+      case interrupt: InterruptedException => error("Error while sleeping")
+      case unknown: Throwable => error("Got this unknown exception: " + unknown)
     }
   }
 }
