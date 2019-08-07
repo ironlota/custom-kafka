25830b9a1d74e9a7d0a074794196af88b86b171c
Ray Andrew <raydreww@gmail.com>
2019-08-07 09:10:23 +0700

    update kafka source

--- core/src/main/scala/kafka/utils/MemoryReader.scala
+++ core/src/main/scala/kafka/utils/MemoryReader.scala
@@ -12,8 +12,8 @@ class MemoryReader extends Runnable with Logging {
       // while (!Thread.currentThread().isInterrupted()) {
       System.gc()
 
-      // sleep for 5 seconds
-      Thread.sleep(5000)
+      // sleep for 12 seconds
+      Thread.sleep(12000)
 
       if (Files.exists(Paths.get("/mnt/extra/holder.txt"))) {
 
